a601f20eae852dcf0668c11903d4ba3364510957
Coco Liu <liuh3@rose-hulman.edu>
2016-01-17 14:31:46 -0500

    modified ITargetClass and TargetClass

--- AsianDomination/src/impl/TargetClass.java
+++ AsianDomination/src/impl/TargetClass.java
@@ -14,74 +14,45 @@ import api.ITargetClass;
 import api.ITargetClassPart;
 
 public class TargetClass implements ITargetClass {
-	private Collection<IClassField> fieldParts;
-	private Collection<IClassMethod> methodParts;
-	private IClassDeclaration declarationPart;
+	private String _className;
+	private Collection<IClassMethod> _methodNameToClassMethod;
+	private Collection<IClassField> _fieldNameToClassField;
 
-	public TargetClass() {
-		fieldParts = new ArrayList<IClassField>();
-		methodParts = new ArrayList<IClassMethod>();
-		declarationPart = null;
+	public TargetClass(String className) {
+		_fieldNameToClassField = new ArrayList<IClassField>();
+		_methodNameToClassMethod = new ArrayList<IClassMethod>();
+		_className = className;
 	}
 
 	@Override
-	public Collection<IClassField> getFieldParts() {
-		return fieldParts;
+	public String getClassName() {
+		return _className;
 	}
 
 	@Override
-	public Collection<IClassMethod> getMethodParts() {
-		return methodParts;
+	public Collection<IClassMethod> getMethods() {
+		return _methodNameToClassMethod;
 	}
 
 	@Override
-	public IClassDeclaration getDeclaration() {
-		return declarationPart;
+	public Collection<IClassField> getFields() {
+		return _fieldNameToClassField;
 	}
 
 	@Override
-	public void addPart(ITargetClassPart part) {
-		if (part instanceof IClassDeclaration) {
-			declarationPart = (IClassDeclaration) part;
-		} else if (part instanceof IClassField) {
-			fieldParts.add((IClassField) part);
-		} else if (part instanceof IClassMethod) {
-			methodParts.add((IClassMethod) part);
-		} else {
-			// FIXME: Throw error later
-			System.err.println("Fix me, you errored.");
-		}
+	public void addClassMethod(IClassMethod classMethod) {
+		this._methodNameToClassMethod.add(classMethod);
+	}
+
+	@Override
+	public void addClassField(IClassField classField) {
+		this._fieldNameToClassField.add(classField);
 	}
 
 	@Override
 	public void accept(IVisitor v) {
-		if (v instanceof UMLOutputStream)
-			doUMLVisiting((UMLOutputStream) v);
-		else if (v instanceof SequenceOutputStream) {
-			SequenceDiagramTraverser sdTraverser = new SequenceDiagramTraverser((SequenceOutputStream) v);
-			sdTraverser.startOutput();
-		}
-	}
+		v.visit(this);
 
-	private void doUMLVisiting(UMLOutputStream v) {
-		v.preVisit(this);
-
-		IClassField lastField = new ClassField(null,null,null,null);
-		for (Iterator<IClassField> fieldIter = fieldParts.iterator(); fieldIter.hasNext();) {
-			lastField = fieldIter.next();
-			lastField.accept(v);
-		}
-
-		// Need to print separation
-		// so we call postVisit on ONE field
-		// doesn't matter which field it is, this should be safe
-		v.postVisit(lastField);
-
-		for (IClassMethod method : methodParts) {
-			method.accept(v);
-		}
-
-		v.postVisit(this);
 	}
 
 }
