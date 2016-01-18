ad14fcfcefc9c268bfa9cade90e7969ee4fc2f70
Collin Trowbridge <trowbrct@rose-hulman.edu>
2016-01-17 20:48:56 -0500

    Remove old outputStream interface. Add method to MethodStatement api.

--- AsianDomination/src/api/IMethodStatement.java
+++ AsianDomination/src/api/IMethodStatement.java
@@ -4,5 +4,6 @@ public interface IMethodStatement extends ITargetClassPart {
 	public String getCallerClass();
 	public String getClassToCall();
 	public String getParameter();
+	public String getMethodName();
 	public int getSequenceLevel();
 }
