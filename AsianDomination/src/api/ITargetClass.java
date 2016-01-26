19231d479e26dc7aa5d99407826d12b6f8f95c47
Runzhi Yang <yangr@rose-hulman.edu>
2016-01-25 23:24:45 -0500

    New containsRelationshipEdge method

--- AsianDomination/src/api/ITargetClass.java
+++ AsianDomination/src/api/ITargetClass.java
@@ -28,4 +28,6 @@ public interface ITargetClass extends ITraverser {
 	public void addDeclarationVisitor(ClassDeclaration classDeclaration);
 
 	public IClassDeclaration getDeclaration();
+	
+	public boolean containsRelationship(RelationshipType edgeType, String subjectClass);
 }
