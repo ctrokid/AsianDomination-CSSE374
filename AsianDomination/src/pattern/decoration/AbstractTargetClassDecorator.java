67e73fe205c34341acab022cb61179660f9542c6
Runzhi Yang <yangr@rose-hulman.edu>
2016-01-26 23:00:49 -0500

    SingletonDecorator

--- AsianDomination/src/pattern/decoration/AbstractTargetClassDecorator.java
+++ AsianDomination/src/pattern/decoration/AbstractTargetClassDecorator.java
@@ -1,19 +1,25 @@
 package pattern.decoration;
 
 import api.ITargetClass;
+import pattern.detection.PATTERN_TYPE;
 
 public abstract class AbstractTargetClassDecorator {
-	
+	protected PATTERN_TYPE pattern;
 	protected String _associatedClassName;
 	protected ITargetClass _decoratedClass;
 
-	public AbstractTargetClassDecorator(String _associatedClassName, ITargetClass _decoratedClass) {
+	public AbstractTargetClassDecorator(PATTERN_TYPE pattern, String _associatedClassName, ITargetClass _decoratedClass) {
+		this.pattern = pattern;
 		this._associatedClassName = _associatedClassName;
 		this._decoratedClass = _decoratedClass;
 	}
 
 	public String getAssociatedClassName() {
 		return _associatedClassName;
+	}
+	
+	public PATTERN_TYPE getPatternType(){
+		return pattern;
 	}
 	
 	
---
+++ AsianDomination/src/pattern/decoration/SingletonDecorator.java
@@ -0,0 +1,13 @@
+package pattern.decoration;
+
+import api.ITargetClass;
+import pattern.detection.PATTERN_TYPE;
+
+public class SingletonDecorator extends AbstractTargetClassDecorator {
+
+	public SingletonDecorator(PATTERN_TYPE pattern, String _associatedClassName, ITargetClass _decoratedClass) {
+		super(pattern, _associatedClassName, _decoratedClass);
+		// TODO Auto-generated constructor stub
+	}
+
+}
