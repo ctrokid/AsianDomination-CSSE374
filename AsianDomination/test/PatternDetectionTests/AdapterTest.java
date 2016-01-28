8d4a820645cc5b5e275441204a5c8604668b92bf
Runzhi Yang <yangr@rose-hulman.edu>
2016-01-28 14:43:05 -0500

    new Adapter test

--- AsianDomination/test/PatternDetectionTests/AdapterTest.java
+++ AsianDomination/test/PatternDetectionTests/AdapterTest.java
@@ -3,19 +3,14 @@ package PatternDetectionTests;
 import impl.ProjectModel;
 import input.InputCommand;
 import input.UMLInputCommand;
-import output.AbstractDiagramOutputStream;
-import output.IDiagramOutputStream;
 import pattern.detection.AdaptorPatternDetector;
-import pattern.detection.DecoratorPatternDetector;
 import pattern.detection.IPatternDetectionStrategy;
 import pattern.detection.SingletonPatternDetector;
-import visitor.Visitor;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.After;
@@ -23,138 +18,77 @@ import org.junit.Before;
 import org.junit.Test;
 
 import api.IProjectModel;
-import construction.UMLAddStrategy;
-import fake.FakeInputCommand;
-import fake.FakeUMLDiagramOutputStream;
+import api.ITargetClass;
 
-@SuppressWarnings("unused")
 public class AdapterTest {
 	private InputCommand _cmd;
-	
+
 	@Before
 	public void setup() {
 		_cmd = null;
 	}
-	
-//	@Test
-//	public void TestUMLForSimpleFactorySystem() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-//		String[] classes = new String[] {
-//				"examples/simple/Animal",
-//				"examples/simple/Cat",
-//				"examples/simple/Dog",
-//				"examples/simple/SimpleAnimalFactory",
-//		};
-//		
-//		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-//		IDiagramOutputStream out = new FakeUMLDiagramOutputStream("", new Visitor());
-//		Field f = AbstractDiagramOutputStream.class.getDeclaredField("_outputStream");
-//		f.setAccessible(true);
-//		f.set(out, bytesOut);
-//		
-//		FakeInputCommand cmd = new FakeInputCommand("", "", classes);
-//		cmd.setOutputStream(out);
-//		cmd.setAddStrategy(new UMLAddStrategy(null));
-//		
-//		IProjectModel model = new ProjectModel(cmd);
-//		model.parseModel();
-//		
-//		String actual = bytesOut.toString();
-//		String expectedFactory = "{SimpleAnimalFactory||+ createAnimal(String) : Animal\\l}";
-//		String expectedAnimalUses = "SimpleAnimalFactory -> Animal[arrowhead = \"vee\",style = \"dashed\"];\n";
-//		String expectedDogUses = "SimpleAnimalFactory -> Dog[arrowhead = \"vee\",style = \"dashed\"];\n";
-//		String expectedCatUses = "SimpleAnimalFactory -> Cat[arrowhead = \"vee\",style = \"dashed\"];\n";
-//		
-//		assertTrue(actual.contains(expectedFactory));
-//		assertTrue(actual.contains(expectedAnimalUses));
-//		assertTrue(actual.contains(expectedDogUses));
-//		assertTrue(actual.contains(expectedCatUses));
-//	}
-//	
-//	@Test
-//	public void TestUMLForSimpleExampleInheritanceSystem() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-//		String[] classes = new String[] {
-//				"examples/simple/Animal",
-//				"examples/simple/Cat"
-//		};
-//		
-//		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-//		IDiagramOutputStream out = new FakeUMLDiagramOutputStream("", new Visitor());
-//		Field f = AbstractDiagramOutputStream.class.getDeclaredField("_outputStream");
-//		f.setAccessible(true);
-//		f.set(out, bytesOut);
-//		
-//		FakeInputCommand cmd = new FakeInputCommand("", "", classes);
-//		cmd.setOutputStream(out);
-//		cmd.setAddStrategy(new UMLAddStrategy(null));
-//		
-//		IProjectModel model = new ProjectModel(cmd);
-//		model.parseModel();
-//		
-//		String actual = bytesOut.toString();
-//		String catExpected = "{Cat|- fur : Fur\\l|+ getNoise(int) : String\\l}";
-//		String animalExpected = "{Animal|# name : String\\l- age : int\\l+ var : double\\l|+ getNoise(int) : String\\l+ run() : void\\l}";
-//		String catToAnimal = "Cat -> Animal[arrowhead = \"empty\",style = \"solid\"];\n";
-//		String catDoesNotAssociateFur = "Cat -> Fur";
-//		String catDoesNotAssociateAnimal = "Cat -> Animal[arrowhead = \"vee\",style = \"solid\"];\n";
-//		
-//		assertTrue(actual.contains(catExpected));
-//		assertTrue(actual.contains(animalExpected));
-//		assertTrue(actual.contains(catToAnimal));
-//		assertFalse(actual.contains(catDoesNotAssociateFur));
-//		assertFalse(actual.contains(catDoesNotAssociateAnimal));
-//	}
-	
+
 	@Test
-	public void TestUMLForProjectSubsystem() {
-		String[] classes = new String[] {
-//				"headfirst/singleton/chocolate/ChocolateBoiler",
-//				"headfirst/singleton/chocolate/ChocolateController"
-//			"examples/simple/SingletonExample",
-//			"examples/decorator/Beverage",
-//			"examples/decorator/CondimentDecorator",
-//			"examples/decorator/Milk",
-//				"java/io/FilterInputStream",
-//				"java/lang/Runtime",
-//				"java/awt/Desktop",
-//				"java/util/Calendar",
-//			"problem/client/App",
-//			"problem/client/IteratorToEnumerationAdapter",
-//			"problem/lib/LinearTransformer",
-//			"java/util/Iterator",
-//			"java/util/Enumeration",
-				"AdapterDummieClasses/Adaptee",
-				"AdapterDummieClasses/Adapter",
-				"AdapterDummieClasses/ITarget"
-		};
+	public void simpleSingletonTest() {
+
+		String[] classes = new String[] { "examples/adapter/Adaptee",
+										  "examples/adapter/Adapter",
+										  "examples/adapter/ITarget"
+										  };
+
+		IProjectModel model = buildModel(classes, "docs/M5/SimpleAdapterTest");
+		SingletonPatternDetector s = new SingletonPatternDetector();
+		s.detectPatterns(model);
+		Iterator<ITargetClass> i = model.getTargetClasses().iterator();
 		
-		List<IPatternDetectionStrategy> detectors = Arrays.asList(new SingletonPatternDetector(), new DecoratorPatternDetector(), new AdaptorPatternDetector());
-		_cmd = new UMLInputCommand("input_output/testProjectSubsystem", "input_output/testProjectSubsystem", classes, detectors);
-		IProjectModel model = new ProjectModel(_cmd);
-		model.parseModel();
+		ITargetClass Adaptee = i.next();
+		assertEquals("red", Adaptee.getColor());
+		
+		ITargetClass Adapter = i.next();
+		assertEquals("red", Adapter.getColor());
+		
+		ITargetClass ITarget = i.next();
+		assertEquals("red", ITarget.getColor());
+		
+		model.printModel();
 	}
-	
+
 //	@Test
-//	public void TestUMLForAnimalCatExample() {
+//	public void IntegrationTest() {
 //		String[] classes = new String[] {
-//			"examples/simple/Animal",
-//			"examples/simple/Cat",
-//			"examples/simple/Dog",
-//			"examples/simple/SimpleAnimalFactory",
-//			"examples/simple/Fur"
+//				 "headfirst/singleton/chocolate/ChocolateBoiler",
+//				 "headfirst/singleton/chocolate/ChocolateController"
+//				 
 //		};
-//		
-//		_cmd = new UMLInputCommand("input_output/testUML", "input_output/testUML", classes);
-//		IProjectModel model = new ProjectModel(_cmd);
-//		model.parseModel();
+//		IProjectModel model = buildModel(classes, "docs/M4/SingletonTest");
+//		model.printModel();
 //	}
 //	
 //	@Test
-//	public void TestGenerationUML() {
-//		LaunchDiagramGenerator.RunGVEdit("input_output/testProjectSubsystem.gv", "input_output/testProjectSubsystem", DiagramFileExtension.PDF);
+//	public void IntegrationTest2() {
+//		String[] classes = new String[] {
+//				  "java/io/FilterInputStream",
+//				  "java/lang/Runtime",
+//				  "java/awt/Desktop",
+//				  "java/util/Calendar"
+//		};
+//		IProjectModel model = buildModel(classes, "docs/M4/AutomatedSingletonTest");
+//		model.printModel();
 //	}
-	
+
 	@After
 	public void tearDown() {
 		_cmd = null;
 	}
+
+	private IProjectModel buildModel(String[] classes, String path) {
+		List<IPatternDetectionStrategy> detectors = Arrays.asList(new AdaptorPatternDetector());
+		_cmd = new UMLInputCommand(path, path, classes, detectors);
+		IProjectModel model = new ProjectModel(_cmd);
+		model.build();
+		return model;
+	}
 }
+
+
+
