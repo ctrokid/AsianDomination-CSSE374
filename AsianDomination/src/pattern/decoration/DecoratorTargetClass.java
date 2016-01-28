5e79fb091b05100bdb0714264557252f108b7933
Collin Trowbridge <trowbrct@rose-hulman.edu>
2016-01-28 01:55:29 -0500

    Move Visitor, rename DecoratorTargetClass.

--- AsianDomination/src/pattern/decoration/DecoratorTargetClass.java
+++ AsianDomination/src/pattern/decoration/DecoratorTargetClass.java
@@ -3,9 +3,9 @@ package pattern.decoration;
 import api.ITargetClass;
 import pattern.detection.PATTERN_TYPE;
 
-public class DecoraterTargetClass extends AbstractTargetClassDecorator {
+public class DecoratorTargetClass extends AbstractTargetClassDecorator {
 
-	public DecoraterTargetClass(PATTERN_TYPE pattern, String _associatedClassName, ITargetClass _decoratedClass) {
+	public DecoratorTargetClass(PATTERN_TYPE pattern, String _associatedClassName, ITargetClass _decoratedClass) {
 		super(pattern, _associatedClassName, _decoratedClass);
 		switch (pattern) {
 			case DECORATOR_COMPONENT: 
@@ -22,6 +22,7 @@ public class DecoraterTargetClass extends AbstractTargetClassDecorator {
 		}
 		
 		setColor("green");
+		setStyle("filled");
 	}
 	
 }
---
+++ AsianDomination/src/visitor/Visitor.java
@@ -0,0 +1,42 @@
+package visitor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Visitor implements IVisitor{
+	private Map<LookupKey, IVisitMethod> _keyToVisitMethodMap;
+
+	public Visitor() {
+		_keyToVisitMethodMap = new HashMap<LookupKey, IVisitMethod>();
+	}
+
+	public void preVisit(ITraverser i) {
+		this.doVisit(VisitType.PreVisit, i);
+	}
+
+	public void visit(ITraverser i) {
+		this.doVisit(VisitType.Visit, i);
+	}
+
+	public void postVisit(ITraverser i) {
+		this.doVisit(VisitType.PostVisit, i);
+	}
+
+	protected void doVisit(VisitType vType, ITraverser t) {
+		LookupKey key = new LookupKey(vType, t.getClass());
+		IVisitMethod m = _keyToVisitMethodMap.get(key);
+
+		if (m != null)
+			m.execute(t);
+	}
+
+	public void addVisit(VisitType visitType, Class<?> clazz, IVisitMethod m) {
+		LookupKey key = new LookupKey(visitType, clazz);
+		_keyToVisitMethodMap.put(key, m);
+	}
+
+	public void removeVisit(VisitType visitType, Class<?> clazz) {
+		LookupKey key = new LookupKey(visitType, clazz);
+		_keyToVisitMethodMap.remove(key);
+	}
+}
