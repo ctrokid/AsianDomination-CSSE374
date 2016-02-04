cff53330d7832477bc04df3673aba27a85b11a12
Runzhi Yang <yangr@rose-hulman.edu>
2016-02-03 20:45:46 -0500

    Solved merge conflits

--- AsianDomination/src/asm/visitor/ClassFieldVisitor.java
+++ AsianDomination/src/asm/visitor/ClassFieldVisitor.java
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-package asm.visitor;
-
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.FieldVisitor;
-
-import api.ITargetClass;
-import impl.ClassField;
-import utils.AsmClassUtils;
-import utils.DotClassUtils.RelationshipType;
-
-public class ClassFieldVisitor extends ClassVisitor {
-	private ITargetClass _targetClass;
-	
-	public ClassFieldVisitor(int api, ITargetClass targetClass) {
-		super(api);
-		_targetClass = targetClass;
-	}
-
-	public ClassFieldVisitor(int api, ClassVisitor decorated, ITargetClass targetClass) {
-		super(api, decorated);
-		_targetClass = targetClass;
-	}
-
-	public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-		FieldVisitor toDecorate = super.visitField(access, name, desc, signature, value);
-		String returnType = AsmClassUtils.GetReturnType(desc);
-
-		if (signature != null) {
-			String sig = AsmClassUtils.parseSignature(signature, false);
-			signature = "\\<" + AsmClassUtils.parseSignature(signature, true) + "\\>";
-			
-			for (String param : sig.split(",")) {
-				_targetClass.addRelationship(RelationshipType.ASSOCIATION, param.trim());
-			}
-		}
-
-		_targetClass.addRelationship(RelationshipType.ASSOCIATION, returnType);
-		_targetClass.addClassField((new ClassField(name, access, signature, returnType)));
-		
-		return toDecorate;
-	};
-=======
 package asm.visitor;
 
 import org.objectweb.asm.ClassVisitor;
@@ -87,5 +44,4 @@ public class ClassFieldVisitor extends ClassVisitor {
 
 		return toDecorate;
 	};
->>>>>>> a1a8608d53853ea8a31a3765e00d308070053e9e
 }
--- AsianDomination/src/asm/visitor/ClassMethodVisitor.java
+++ AsianDomination/src/asm/visitor/ClassMethodVisitor.java
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-package asm.visitor;
-
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor;
-
-import api.IClassMethod;
-import api.ITargetClass;
-
-import impl.ClassMethod;
-import jdk.internal.org.objectweb.asm.Opcodes;
-import utils.AsmClassUtils;
-import utils.DotClassUtils.RelationshipType;
-
-public class ClassMethodVisitor extends ClassVisitor {
-	private ITargetClass _targetClass;
-
-	public ClassMethodVisitor(int api, ITargetClass targetClass) {
-		super(api);
-		this._targetClass = targetClass;
-	}
-
-	public ClassMethodVisitor(int api, ClassVisitor decorated, ITargetClass targetClass) {
-		super(api, decorated);
-		this._targetClass = targetClass;
-	}
-
-	@Override
-	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-		MethodVisitor toDecorate = super.visitMethod(access, name, desc, signature, exceptions);
-
-		String type = AsmClassUtils.GetReturnType(desc);
-
-		IClassMethod classMethod = new ClassMethod(name, desc, access, type);
-		
-		String args = AsmClassUtils.GetArguments(desc, false);
-		MethodVisitor _decorator = new MethodAssociationVisitor(Opcodes.ASM5, toDecorate, _targetClass, args, classMethod);
-		
-		//FIXME:TODO Watch for UML bug totally ignoring static now
-		if (!name.equals("clinit")) {
-			_targetClass.addClassMethod(classMethod);
-			if (!name.contains("<")) {
-				_targetClass.addRelationship(RelationshipType.USES, type);
-			}
-		}
-		return _decorator;
-	}
-
-=======
 package asm.visitor;
 
 import org.objectweb.asm.ClassVisitor;
@@ -97,5 +48,4 @@ public class ClassMethodVisitor extends ClassVisitor {
 		return _decorator;
 	}
 
->>>>>>> a1a8608d53853ea8a31a3765e00d308070053e9e
 }
