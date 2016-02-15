b366d4e463afecc2de3c77780923aa56a16c0d8c
Runzhi Yang <yangr@rose-hulman.edu>
2016-02-15 00:16:56 -0500

    Defaulte Decorater

---
+++ AsianDomination/src/pattern/decoration/GraphVizDefaultSytleDecorator.java
@@ -0,0 +1,13 @@
+package pattern.decoration;
+
+import api.ITargetClass;
+import pattern.detection.PATTERN_TYPE;
+
+public class GraphVizDefaultSytleDecorator extends GraphVizStyleTargetClass {
+	
+	public GraphVizDefaultSytleDecorator(ITargetClass _decoratedClass) {
+		super(PATTERN_TYPE.GRAPHVIZ_DEFAULT, "", _decoratedClass);
+//		setPatternString("");
+	}
+
+}
