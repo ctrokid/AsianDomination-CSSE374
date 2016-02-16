e00959fa5e88ea03f8334a332f95f7348169d197
Collin Trowbridge <trowbrct@rose-hulman.edu>
2016-02-16 15:11:25 -0500

    Remove unused Decorator classes.

--- AsianDomination/src/pattern/decoration/GraphVizDefaultSytleDecorator.java
+++ AsianDomination/src/pattern/decoration/GraphVizDefaultSytleDecorator.java
@@ -4,10 +4,9 @@ import api.ITargetClass;
 import pattern.detection.PATTERN_TYPE;
 
 public class GraphVizDefaultSytleDecorator extends GraphVizStyleTargetClass {
-	
+
 	public GraphVizDefaultSytleDecorator(ITargetClass _decoratedClass) {
 		super(PATTERN_TYPE.GRAPHVIZ_DEFAULT, "", _decoratedClass);
-//		setPatternString("");
 	}
 
 }
