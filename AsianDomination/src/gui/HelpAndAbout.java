0ae0b0ed8429af0970a0f4f871bf9f538c35d8c3
Coco Liu <liuh3@rose-hulman.edu>
2016-02-18 16:19:24 -0500

    centered the title in help and about

--- AsianDomination/src/gui/HelpAndAbout.java
+++ AsianDomination/src/gui/HelpAndAbout.java
@@ -26,7 +26,7 @@ public class HelpAndAbout extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HelpAndAbout window = new HelpAndAbout(barTypes.ABOUT);
+					HelpAndAbout window = new HelpAndAbout(barTypes.HELP);
 					window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -49,7 +49,7 @@ public class HelpAndAbout extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		setBounds(100, 100, 800, 270);
+		setBounds(100, 100, 800, 260);
 		if(this.type.equals(barTypes.ABOUT)){
 			setBounds(100, 100, 500, 150);
 		}
@@ -61,8 +61,8 @@ public class HelpAndAbout extends JFrame {
 	}
 
 	private void setupJLabels() {
-		String helpString = "<html> Help <br> "
-				+ "Load configuration: User is able to generate the diagram based on their personal preference. <br>"
+		String helpString = "<html> <div style='text-align: center;'> Help <html> <br> "
+				+ "<div style='text-align: left;'> Load configuration: User is able to generate the diagram based on their personal preference. <br>"
 				+ "Folder Path: The path must ends with \\src in order for the program to run <br>"
 				+ "Classes: The class needs to be in the project directory or in one of the java default package. <br>"
 				+ "Output Path: The path must be under the project. <br> "
@@ -76,7 +76,7 @@ public class HelpAndAbout extends JFrame {
 				+ "Composite detection: User is able to enforce add-remove is used in the composite pattern or not. <br>"
 				+ "Generate: it will write to the current configuration file, and remember user’s preference. <html>";
 		JLabel helpLabel = new JLabel(helpString);
-		String aboutString = "<html>About <br> The copyright of this project belongs to Haolin (Coco) Liu, <br> Collin Trowbridge, and Runzhi Yang, which is designed for a  <br> seven-week long project forCSSE374, Software Design, with <br> Chandan Raj Rupakheti at Rose-Hulman Institute of Technology.<html>";
+		String aboutString = "<html> <div style='text-align: center;'> About <br> The copyright of this project belongs to Haolin (Coco) Liu, <br> Collin Trowbridge, and Runzhi Yang, which is designed for a  <br> seven-week long project forCSSE374, Software Design, with <br> Chandan Raj Rupakheti at Rose-Hulman Institute of Technology.<html>";
 		JLabel aboutLabel = new JLabel(aboutString);
 		typeMap.put(barTypes.ABOUT, aboutLabel);
 		typeMap.put(barTypes.HELP, helpLabel);
